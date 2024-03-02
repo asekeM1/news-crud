25400c5691f057ddacbe64388c774545fd351d68
aseke <asanmaratulu@gmail.com>
2024-03-03 01:40:56 +0500

    added login and logout login

--- prod/website/views.py
+++ prod/website/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Успешно! Вы успешно авторизовались!')
+            return redirect('home')
+        else:
+            messages.success(request, 'Упс! Что-то пошло не так..')
+            return redirect('home')
     return render(request, 'home.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'Вы успешно вышли из профиля')
+    return redirect('home')
+
