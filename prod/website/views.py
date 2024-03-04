2428d2595b65833a4ef21f6abf7c43063aadd62f
aseke <asanmaratulu@gmail.com>
2024-03-05 00:44:10 +0500

    added create, update, delete logics, still need DRY method..

--- prod/website/views.py
+++ prod/website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 def home(request):
     records = Record.objects.all()
@@ -42,3 +42,46 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form':form})
 
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        personal_data = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'personal_data':personal_data})
+    else:
+        messages.success(request, 'Вы должны войти в систему прежде чем войти сюда!')
+        return redirect('home')
+    
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        personal_data = Record.objects.get(id=pk)
+        personal_data.delete()
+        messages.success(request, 'Пользователь успешно удален!')
+        return redirect('home')
+    else:
+        messages.success(request, 'Вы должны войти в систему прежде чем сделать сюда!')
+        return redirect('home')
+    
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, 'Пользователь успешно добавлен!')
+                return redirect('home')
+        return render(request, 'add_record.html', {'form': form})
+    else:
+        messages.success(request, 'Вы должны войти в систему прежде чем сделать сюда!')
+        return redirect('home')
+    
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_data = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_data)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Данные успешно обновлены!')
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form})
+    else:
+        messages.success(request, 'Вы должны войти в систему прежде чем сделать сюда!')
+        return redirect('home')
